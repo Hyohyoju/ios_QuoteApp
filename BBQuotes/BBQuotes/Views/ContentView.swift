3195a14b79036ba2834545be94f49723025fbad2
이효주 <julie08051@naver.com>
2024-12-18 19:01:38 +0900

    no message

---
+++ BBQuotes/BBQuotes/Views/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  BBQuotes
+//
+//  Created by 이효주 on 12/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            QuoteView(show: "Breaking Bad")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Breaking Bad", systemImage: "tortoise")
+                }
+            
+            QuoteView(show: "Better Call Saul")
+                .toolbarBackgroundVisibility(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Better Call Saul", systemImage: "briefcase")
+                }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
